b84030fe7e472cf4e762ee201ff6b9631ea43964
triyashakunduofficial@gmail.com <triyashakunduofficial@gmail.com>
2022-11-28 10:26:19 +0000

    commit

--- models/AgeWise.sql
+++ models/AgeWise.sql
@@ -2,6 +2,6 @@ ALTER TABLE patient
 ADD age INT;
 
 UPDATE employee
-SET age = DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), date_of_birth)), '%Y') + 0;
+SET age = DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), DOB)), '%Y') + 0;
 
 SELECT * FROM patient
