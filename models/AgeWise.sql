983a2020a5ebc03f77ced683ca27e879c473e248
triyashakunduofficial@gmail.com <triyashakunduofficial@gmail.com>
2022-11-28 08:18:59 +0000

    commit

--- models/AgeWise.sql
+++ models/AgeWise.sql
@@ -1,3 +1,3 @@
-SELECT * FROM lab_two.Patient
-SELECT DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),'DOB')), '%Y') 
- + 0 AS age
+SELECT *, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(), 'DOB')), '%Y') + 0 AS age
+FROM lab_two.Patient
+
