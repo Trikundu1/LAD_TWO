f1242e3628797a7bff480537cb5081f498bd4f12
triyashakunduofficial@gmail.com <triyashakunduofficial@gmail.com>
2022-11-28 09:57:27 +0000

    commit

--- models/LocationWise.sql
+++ models/LocationWise.sql
@@ -1,2 +1,23 @@
-SELECT * FROM lab_two.Patient
-ORDER BY Patient.city
+with stg_Patient as (
+    Patient_id ,Patient_name ,DOB ,SpouseorParent_name,Gender ,Married ,Address ,City,Pincode ,Phone
+    from lab_two.Patient
+),
+final as (
+    select
+        stg_Patient.City, 
+        stg_Patient.Patient_id,
+        stg_Patient.Patient_name,
+        stg_Patient.DOB,
+        stg_Patient.Gender,
+        stg_Patient.Married,
+        stg_Patient.SpouseorParent_name,
+        stg_Patient.Address,
+        stg_Patient.Phone
+    from stg_Patient
+    order by stg_Patient.City
+)
+select * from final
+
+
+
+)
